12f0df028981106356a517440619da492cae6832
Andrew Woan <andrewwoan@gmail.com>
2026-09-16 15:08:43 -0400

    final files

---
+++ final files/main.js
@@ -0,0 +1,35 @@
+const menuIcon = document.querySelector(".menu-svg");
+const mobileMenu = document.querySelector(".mobile-nav-menu");
+
+menuIcon.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
+});
+
+const mobileQuery = window.matchMedia("(max-width: 900px)");
+
+mobileQuery.addEventListener("change", (e) => {
+  if (!e.matches) {
+    mobileMenu.classList.remove("active");
+  }
+});
+
+
+const navHeader = document.querySelector(".nav-header");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+  const currentScrollY = window.scrollY;
+
+  if (mobileMenu.classList.contains("active")) {
+    lastScrollY = currentScrollY;
+    return;
+  }
+
+  if (currentScrollY > lastScrollY) {
+    navHeader.classList.add("hidden");
+  } else {
+    navHeader.classList.remove("hidden");
+  }
+
+  lastScrollY = currentScrollY;
+});
